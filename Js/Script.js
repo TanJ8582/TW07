95bf2e874237304e7065928bf037fabac96949c5
TanJ8582 <202310891@fit.edu.ph>
2024-06-15 11:58:53 +0800

    Add files via upload

---
+++ Js/Script.js
@@ -0,0 +1,55 @@
+
+
+// $name = "Jusine Louis D.Tan";
+// alert($name);
+
+
+// $name = "jp lugo";
+// alert($name);//alert box
+
+
+// document.getElementById("Text").innerHTML = $name;
+// console.log($name);
+
+
+// let firstname= "Relato";
+// alert("Hi, " + firstname);
+// alert("are u retarded " + firstname + "?")
+
+
+// let num1= "12feutech";
+// let num2= 2;
+// num1 = parseInt(num1)
+// alert(num1 + num2); 
+
+
+// let num1= "15.99";
+// let num2= "0.2";
+// alert(parseInt(num1) + num2);
+
+
+// let num = prompt("Enter an integer : ","0");
+// document.getElementById("someId").innerHTML = num;
+
+// let num = prompt("Enter an integer : ","0");
+// document.writeIn(" Num = " + num + "<br>");
+// document.writeIn(" Num = " + num + "<br>");
+
+let num1 = parseFloat(prompt("Enter number 1:")); 
+let num2 = parseFloat(prompt("Enter number 2:")); 
+let num3 = parseFloat(prompt("Enter number 3:")); 
+let num4 = parseFloat(prompt("Enter number 4:")); 
+let num5 = parseFloat(prompt("Enter number 5:")); 
+document.write("<h1>Entered Numbers: </h1>"); 
+document.write("<ul>"); 
+document.write(`<li>${num1}</li>`); 
+document.write(`<li>${num2}</li>`); 
+document.write(`<li>${num3}</li>`); 
+document.write(`<li>${num4}</li>`); 
+document.write(`<li>${num5}</li>`); 
+document.write("</ul>"); 
+let sum = num1 + num2 + num3 + num4 + num5; let average = sum / 5; 
+document.write(`<h1>Sum:</h1>`); 
+document.write(`<p>${sum}</p>`); 
+document.write(`<h2>Average:</h2>`); 
+document.write(`<p>${average}</p>`);
